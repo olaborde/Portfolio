077ecf4a5a081833bb6d8fc8ef535d3d48af528b
olaborde <oml007@live.com>
2018-12-09 22:46:51 -0500

    added assets

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+$(document).ready(function () {
+    $('.header').height($(window).height());
+})
+
+
+// Sroll effect
+
+// $(".navbar a").click(function () {
+//     $("body,html").animate({
+//         scrollTop: $("#" + $(this).data('value')).offset().top
+//     }, 1000)
+
+// })
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
